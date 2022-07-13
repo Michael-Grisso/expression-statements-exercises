1db25f0b502c856fcaa854c9b7df7e04e6392019
Michael <michaelgrisso2011@gmail.com>
2022-07-13 11:54:02 -0400

    javascript

---
+++ js/rectanglearea.js
@@ -0,0 +1,7 @@
+function main(length, width) {
+    let area = length * width;
+    let perimeter = (length + width) * 2;
+    console.log(`${area}\n${perimeter}`);
+
+}
+main(30, 40);
---
+++ js/usdtobgn.js
@@ -0,0 +1,7 @@
+function main(usd) {
+    let bgn = 1.79549;
+    let result = usd * bgn;
+    console.log(`${result.toFixed(2)} BGN`);
+    
+}
+main(20);
